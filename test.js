73a5bc248add71901c62d3d4df7b566bbad55e27
summer <wpsumsun@126.com>
2017-03-07 00:44:30 +0800

    task7

--- test.js
+++ test.js
@@ -1,9 +1,31 @@
-function getRandColor(){
-    var dict="0123456789abcdef";
-    var colorStr="#",n;
-    for(var i=0;i<6;i++){
-        n=Math.floor( Math.random()*(dict.length) );
-        colorStr+=dict[n];
+
+
+
+
+
+
+function friendlyDate(time){
+    var offsetTime=new Date().getTime()-time;
+    var mins=offsetTime/(60*1000);
+    var hours=offsetTime/(60*60*1000);
+    var days=offsetTime/(24*60*60*1000);
+    var  mons=offsetTime/(30*24*60*60*1000);
+    var  years=offsetTime/(12*30*24*60*60*1000);
+    if(mins<1){
+        return "刚刚";
     }
-    return colorStr
+    else if(hours<1){
+        return "三分钟前";
+    }
+    else if(days<1){
+        return "8小时前";
+    }
+    else if(mons<1){
+        return "3天前";
+    }
+    else if(years<1){
+        return "2个月前";
+    }else{
+        return "8年前";
+    }
 }
