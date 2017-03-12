a71262b0989276a27bc4cf99c2e930ebcbec8522
summer <wpsumsun@126.com>
2017-03-13 00:27:43 +0800

    task10

--- test.js
+++ test.js
@@ -1,16 +1,7 @@
-
-
-
-
-
-
-
-function isValidPassword(str){
-    if(str.length<6||str.length>20||/\W/.test(str)){return false}
-    var count=0;
-    if(/[A-Z]/.test(str)){count++}
-    if(/[a-z]/.test(str)){count++}
-    if(/[0-9]/.test(str)){count++}
-    if(/_/.test(str)){count++}
-    return count>=2?true:false;
-}
+！
+<script>
+var ul=document.querySelector('.ul');
+ul.addEventListener("click",function(e){
+    console.log(e.target.innerText);
+})
+</script>
