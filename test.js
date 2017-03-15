b660ec02a83ab78a6a4ff6d11ec113d85fa768c0
summer <wpsumsun@126.com>
2017-03-16 00:56:25 +0800

    ajax

--- test.js
+++ test.js
@@ -1,39 +1,7 @@
-<script>
-    var Car = (function(){
-        var speed = 0;
-        function setSpeed(s){
-                speed = s
-        }
-        function getSpeed(){
-            return speed
-        }
-        function accelerate(){
-            speed+=10;
-        }
-        function decelerate(){
-            speed-=10;
-        }
-        function getStatus(){
-            if(speed>0){
-                return "running"
-            }else{
-                return "stop"
-            }
-        }
-        return {
-                setSpeed: setSpeed,
-        }
-    })()
-    Car.setSpeed(30);
-    Car.getSpeed(); //30
-    Car.accelerate();
-    Car.getSpeed(); //40;
-    Car.decelerate();
-    Car.decelerate();
-    Car.getSpeed(); //20
-    Car.getStatus(); // 'running';
-    Car.decelerate();
-    Car.decelerate();
-    Car.getStatus();  //'stop';
-    //Car.speed;  //error
-</script>
+
+
+
+
+
+
+
