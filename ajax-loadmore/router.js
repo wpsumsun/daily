fe49eb0c486c4df497179ac06e12f90539624f94
summer <wpsumsun@126.com>
2017-03-19 21:43:14 +0800

    ajax

---
+++ ajax-loadmore/router.js
@@ -0,0 +1,11 @@
+
+
+app.get('/loadmore',function(req,res){
+	var newsIndex=req.query.index;
+	var len=req.query.length;
+	var data=[];
+	for(var i=0;i<len;i++){
+		data.push('新闻'+ (parseInt(newsIndex)+i) );
+	}
+	res.send(data);
+})
