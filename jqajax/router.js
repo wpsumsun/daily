0f3c5662c81fb9aef84209f71a9483ea3698e542
summer <wpsumsun@126.com>
2017-03-28 22:55:58 +0800

    jqajax

--- jqajax/router.js
+++ jqajax/router.js
@@ -59,3 +59,16 @@ app.get('/user', function(req, res) {
 		username: '饥人谷'
 	});
 });
+
+
+app.get('/loadmore',function(req,res){
+	var newsIndex=req.query.index;
+	var len=req.query.length;
+	var data=[];
+	for(var i=0;i<len;i++){
+		data.push('新闻'+ (parseInt(newsIndex)+i) );
+	}
+	setTimeout(function() {
+		res.send(data);
+	},2000 );
+})
