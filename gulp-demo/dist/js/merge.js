455654ed48879b8279bacc6eea560374c87dc3df
Summer <wpsumsun@126.com>
2017-05-23 11:47:03 +0800

    gulp demo

---
+++ gulp-demo/dist/js/merge.js
@@ -0,0 +1,1 @@
+$("body").on("click",".wrap img",function(){console.log($(this).index())}),$(".wrap img").hover(function(){$(this).css("width","300px")},function(){$(this).css("width","240px")});
--- gulp-demo/gulpfile.js
+++ gulp-demo/gulpfile.js
@@ -1,40 +1,40 @@
 var gulp=require('gulp');
 
-// var cssnano=require('gulp-cssnano');
-// var concat=require('gulp-concat');
-// var uglify=require('gulp-uglify');
-// var imgmin=require('gulp-imagemin');
-// var clean=require('gulp-clean');
+var cssnano=require('gulp-cssnano');
+var concat=require('gulp-concat');
+var uglify=require('gulp-uglify');
+var imgmin=require('gulp-imagemin');
+var clean=require('gulp-clean');
 
 //同步刷新
 
 var browserSync=require('browser-sync').create();
 var fs=require('fs');
 
-// gulp.task('css',function(){
-//     gulp.src('./src/css/*.css')
-//         .pipe(concat('index-merge.css'))
-//         .pipe(cssnano())
-//         .pipe(gulp.dest('dist/css/'))
-// });
+gulp.task('css',function(){
+    gulp.src('./src/css/*.css')
+        .pipe(concat('index-merge.css'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css/'))
+});
 
-// gulp.task('js',function(){
-//     gulp.src('./src/js/*.js')
-//         .pipe(concat('merge.js'))
-//         .pipe(uglify())
-//         .pipe(gulp.dest('dist/js/'))
-// })
+gulp.task('js',function(){
+    gulp.src('./src/js/*.js')
+        .pipe(concat('merge.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+})
 
-// gulp.task('img',function(){
-//     gulp.src('./src/images/*')
-//         .pipe(imgmin())
-//         .pipe(gulp.dest('dist/images/'))
-// })
+gulp.task('img',function(){
+    gulp.src('./src/images/*')
+        .pipe(imgmin())
+        .pipe(gulp.dest('dist/images/'))
+})
 
-// gulp.task('clean',function(){
-//     gulp.src('./dist/*',{read:false})
-//         .pipe(clean())
-// })
+gulp.task('clean',function(){
+    gulp.src('./dist/*',{read:false})
+        .pipe(clean())
+})
 
 //同步刷新
 gulp.task('reload',function(){
